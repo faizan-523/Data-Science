d0bfc1cb9586625ecc7b9f4cc03bb4e4976db0fe
faizan-523 <faizan.7.ist@gmail.com>
2026-06-30 01:07:25 +0500

    Outliers

--- outliers.py
+++ outliers.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-np.arr = np.array([1, 2, 3, 4, 5,6,7,4,8,9,20,21,17,18,15,10,23,25, 100])
+arr = np.array([1, 2, 3, 4, 5,6,7,4,8,9,20,21,17,18,15,10,23,25, 100])
 
-Q1 = np.percentile(np.arr, 25)
+Q1 = np.percentile(arr, 25)
 
-Q3 = np.percentile(np.arr, 75)
+Q3 = np.percentile(arr, 75)
 IQR = Q3 - Q1
 
 lower_bound = Q1 - 1.5 * IQR
@@ -18,11 +20,15 @@ print("IQR:", IQR)
 
 l = []
 
-for i in np.arr:
+for i in arr:
     if i >= lower_bound and i <= upper_bound:
         l.append(i)
 
 arr2 = np.array(l)
 
-print("Original Array:", np.arr)
+print("Original Array:", arr)
 print("Filtered Array:", arr2)
+
+sns.boxplot(arr2)
+plt.show()
+
